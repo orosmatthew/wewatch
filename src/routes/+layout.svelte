aa0ffe790461f2b1fae23aaf59e50c989ab2a42f
orosmatthew <orosmatthew@pm.me>
2023-05-10 12:13:04 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import 'bootstrap-dark-5/dist/css/bootstrap-dark.css';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		await import('bootstrap');
+	});
+</script>
+
+<div class="container">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { defineCustomElements } from '@vime/core';
+	import '@vime/core/themes/default.css';
+	if (browser) {
+		defineCustomElements();
+	}
+</script>
+
+<h1>WeWatch</h1>
+
+<div style="width:50%">
+	<vm-player>
+		<vm-youtube video-id="DyTCOwB0DVw" />
+		<vm-default-ui />
+	</vm-player>
+</div>
