b683e6c7a290663b3814f7784e5b1d32109598d9
orosmatthew <orosmatthew@pm.me>
2023-05-17 00:52:01 -0400

    Remove bootstrap js import from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,11 @@
 <script lang="ts">
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import 'bootstrap-icons/font/bootstrap-icons.min.css';
-	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 	$socketUrl = data.socketUrl;
-
-	onMount(async () => {
-		await import('bootstrap');
-	});
 </script>
 
 <div class="container">
